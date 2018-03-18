53cc8f00c109d1c8e2799d34d236f0c7083c6f24
Alexandreil <36792981+Alexandreil@users.noreply.github.com>
2018-03-18 14:13:19 +0300

    Update plate.py

--- plate.py
+++ plate.py
@@ -1,6 +1,15 @@
 from tkinter import *
 import random
 
+def fon():
+	y = 0
+	while y < 650:
+		x = 0
+		while x < 650:
+			canvas.create_rectangle(x, y, x+60, y+57, outline="#C69515", fill = "#ECD598")
+			x+=60
+		y+=57
+#Функция начала игры, размещаем плиты
 def start_click(event):
 	plt0.place(x=181, y=140)
 	plt1.place(x=241, y=140)
@@ -18,14 +27,230 @@ def start_click(event):
 	plt13.place(x=241, y=311)
 	plt14.place(x=301, y=311)
 	plt15.place(x=361, y=311)
-	btn1.grid_forget()
-	y = 0
-	while y < 650:
-		x = 0
-		while x < 650:
-			canvas.create_rectangle(x, y, x+60, y+57, outline="#C69515", fill = "#ECD598")
-			x+=60
-		y+=57
+	btn1.destroy()
+	fon()
+
+def res_click(event):
+	fon()
+	btn2.destroy()
+
+#Функции для окрашивания плит в случае правильной последовательности и сброс предыдущих ответов в случае ошибки
+def click0(event):
+	plt0.configure(bg="green")
+
+def click1(event):
+	if plt0["bg"] == "green":
+		plt1.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+
+def click2(event):
+	if plt1["bg"] == "green":
+		plt2.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+
+def click3(event):
+	if plt2["bg"] == "green":
+		plt3.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+
+def click4(event):
+	if plt3["bg"] == "green":
+		plt4.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+
+def click5(event):
+	if plt4["bg"] == "green":
+		plt5.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+
+def click6(event):
+	if plt5["bg"] == "green":
+		plt6.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+
+def click7(event):
+	if plt6["bg"] == "green":
+		plt7.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+
+def click8(event):
+	if plt7["bg"] == "green":
+		plt8.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+
+def click9(event):
+	if plt8["bg"] == "green":
+		plt9.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+		plt8.configure(bg="#E3E0D5")
+
+def click10(event):
+	if plt9["bg"] == "green":
+		plt10.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+		plt8.configure(bg="#E3E0D5")
+		plt9.configure(bg="#E3E0D5")
+
+def click11(event):
+	if plt10["bg"] == "green":
+		plt11.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+		plt8.configure(bg="#E3E0D5")
+		plt9.configure(bg="#E3E0D5")
+		plt10.configure(bg="#E3E0D5")
+
+def click12(event):
+	if plt11["bg"] == "green":
+		plt12.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+		plt8.configure(bg="#E3E0D5")
+		plt9.configure(bg="#E3E0D5")
+		plt10.configure(bg="#E3E0D5")
+		plt11.configure(bg="#E3E0D5")
+
+def click13(event):
+	if plt12["bg"] == "green":
+		plt13.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+		plt8.configure(bg="#E3E0D5")
+		plt9.configure(bg="#E3E0D5")
+		plt10.configure(bg="#E3E0D5")
+		plt11.configure(bg="#E3E0D5")
+		plt12.configure(bg="#E3E0D5")
+
+def click14(event):
+	if plt13["bg"] == "green":
+		plt14.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+		plt8.configure(bg="#E3E0D5")
+		plt9.configure(bg="#E3E0D5")
+		plt10.configure(bg="#E3E0D5")
+		plt11.configure(bg="#E3E0D5")
+		plt12.configure(bg="#E3E0D5")
+		plt13.configure(bg="#E3E0D5")
+
+def click15(event):
+	if plt14["bg"] == "green":
+		plt15.configure(bg="green")
+	else:
+		plt0.configure(bg="#E3E0D5")
+		plt1.configure(bg="#E3E0D5")
+		plt2.configure(bg="#E3E0D5")
+		plt3.configure(bg="#E3E0D5")
+		plt4.configure(bg="#E3E0D5")
+		plt5.configure(bg="#E3E0D5")
+		plt6.configure(bg="#E3E0D5")
+		plt7.configure(bg="#E3E0D5")
+		plt8.configure(bg="#E3E0D5")
+		plt9.configure(bg="#E3E0D5")
+		plt10.configure(bg="#E3E0D5")
+		plt11.configure(bg="#E3E0D5")
+		plt12.configure(bg="#E3E0D5")
+		plt13.configure(bg="#E3E0D5")
+		plt14.configure(bg="#E3E0D5")
+	if plt15["bg"] == "green":
+		plt0.destroy()
+		plt1.destroy()
+		plt2.destroy()
+		plt3.destroy()
+		plt4.destroy()
+		plt5.destroy()
+		plt6.destroy()
+		plt7.destroy()
+		plt8.destroy()
+		plt9.destroy()
+		plt10.destroy()
+		plt11.destroy()
+		plt12.destroy()
+		plt13.destroy()
+		plt14.destroy()
+		plt15.destroy()
+		canvas.create_text(303, 260, text = win, fill = "black", font =("Helvetica", "28"))
+		btn2.place(x=216, y=342)
 
 root = Tk()
 
@@ -35,7 +260,7 @@ root.title("Порядок")
 canvas = Canvas(root, width = 600, height = 600, bg = "#E6C67D")
 canvas.pack()
 
-#Вступительный текст
+#Вступительный текст и конечный
 text = '''Рад Вас видеть в моей мини игре “Порядок”.
 
  Смысл игры заключается в том, что необходимо нажать
@@ -44,7 +269,8 @@ text = '''Рад Вас видеть в моей мини игре “Поряд
 Замечание: эта игра поможет Вам развить Ваше внимание.
 
 Приятной игры!'''
-canvas.create_text(330, 260, text = text, fill = "#EAECFF", font =("Helvetica", "14"))
+win = '''Ты победил!'''
+canvas.create_text(330, 260, text = text, fill = "black", font =("Helvetica", "14"))
 
 #Создаем плитки с номерами 
 plt0 = Button(root, text = "1", width = 7, height = 3, bg = "#E3E0D5")
@@ -64,9 +290,30 @@ plt13 = Button(root, text = "14", width = 7, height = 3, bg = "#E3E0D5")
 plt14 = Button(root, text = "15", width = 7, height = 3, bg = "#E3E0D5")
 plt15 = Button(root, text = "16", width = 7, height = 3, bg = "#E3E0D5")
 
+#В случае нажатия на плитку вызываем функцию
+plt0.bind("<Button-1>", click0)
+plt1.bind("<Button-1>", click1)
+plt2.bind("<Button-1>", click2)
+plt3.bind("<Button-1>", click3)
+plt4.bind("<Button-1>", click4)
+plt5.bind("<Button-1>", click5)
+plt6.bind("<Button-1>", click6)
+plt7.bind("<Button-1>", click7)
+plt8.bind("<Button-1>", click8)
+plt9.bind("<Button-1>", click9)
+plt10.bind("<Button-1>", click10)
+plt11.bind("<Button-1>", click11)
+plt12.bind("<Button-1>", click12)
+plt13.bind("<Button-1>", click13)
+plt14.bind("<Button-1>", click14)
+plt15.bind("<Button-1>", click15)
+
 #Создаем кнопку начала игры
 btn1 = Button(root, text = "Начать игру", width = 10, height = 1, bg = "#CA9B1E")
 btn1.place(x=248, y=542)
 btn1.bind("<Button-1>", start_click)
+
+btn2 = Button(root, width = 10, height = 1, bg = "#CA9B1E", text = "Повторить", font =("Helvetica", "20"))
+btn2.bind("<Button-1>", res_click)
 
 root.mainloop()
